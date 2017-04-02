e3a06049faaea34cfac5e05eb99cef61786112a3
Pieter Mantel <4pietermantel@gmail.com>
2017-04-02 10:22:01 +0200

    entities - 2

--- src/dev/pietermantel/main/entitys/Player.java
+++ src/dev/pietermantel/main/entitys/Player.java
@@ -1,20 +1,35 @@
 package dev.pietermantel.main.entitys;
 
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
 
+import javax.swing.JFrame;
+
+import dev.pietermantel.main.Game;
 import dev.pietermantel.main.entitys.components.HealthComponent;
+import dev.pietermantel.main.entitys.components.KeyInputComponent;
 import dev.pietermantel.main.entitys.components.PositionComponent;
 import dev.pietermantel.main.entitys.components.TextureComponent;
 import dev.pietermantel.main.imgloader.Assets;
+import dev.pietermantel.tools.Clamp;
 
 public class Player extends Entity{
 	HealthComponent health;
 	PositionComponent position;
 	TextureComponent texture;
+	KeyInputComponent keyInput;
 	
 
 	@Override
 	public void tick() {
+		if(keyInput.isPressed(KeyEvent.VK_RIGHT)) position.setX(position.getX()+5);
+		if(keyInput.isPressed(KeyEvent.VK_LEFT)) position.setX(position.getX()-5);
+		if(keyInput.isPressed(KeyEvent.VK_UP)) position.setY(position.getY()-5);
+		if(keyInput.isPressed(KeyEvent.VK_DOWN)) position.setY(position.getY()+5);
+		
+		position.setX(Clamp.clamp(position.getX(), 0, Game.frameWidth - 64));
+		position.setY(Clamp.clamp(position.getY(), 0, Game.frameHeight - 64));
+		
 		texture.setX(position.getX());
 		texture.setY(position.getY());
 	}
@@ -24,9 +39,11 @@ public class Player extends Entity{
 		texture.render(g);
 	}
 	
-	public Player(float x, float y) {
+	public Player(float x, float y, JFrame frame) {
 		health = new HealthComponent(100);
 		position = new PositionComponent(x,y);
 		texture = new TextureComponent(Assets.player_left, position.getX(), position.getY());
+		keyInput = new KeyInputComponent();
+		frame.addKeyListener(keyInput);
 	}
 }
--- src/dev/pietermantel/main/entitys/components/KeyInputComponent.java
+++ src/dev/pietermantel/main/entitys/components/KeyInputComponent.java
@@ -1,5 +1,30 @@
 package dev.pietermantel.main.entitys.components;
 
-public class KeyInputComponent {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+public class KeyInputComponent implements KeyListener{
+	
+	private boolean[] pressed;
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		pressed[e.getKeyCode()] = true;
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		pressed[e.getKeyCode()] = false;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {}
+	
+	public KeyInputComponent() {
+		pressed = new boolean[222];
+	}
+	
+	public boolean isPressed(int key) {
+		return pressed[key];
+	}
 }
